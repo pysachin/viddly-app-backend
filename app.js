4f1eeb93421da262c6110c376adb0f2ddc2bb3d4
pysachin <71167466+pysachin@users.noreply.github.com>
2021-07-25 11:05:41 +0530

    updated port information

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-
-const express =  require('express');
+const express = require("express");
 const app = express();
-const winston = require('winston');
+const winston = require("winston");
 
-require('./startup/validation')();
-require('./startup/logging')();
-require('./startup/routes')(app);
-require('./startup/db')();
-require('./startup/config')();
-require('./startup/prod')(app);
+require("./startup/validation")();
+require("./startup/logging")();
+require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/config")();
+require("./startup/prod")(app);
 
 // PORT
 const port = process.env.PORT || 3000;
-const server = app.listen(3000, () => winston.info(`listening on port ${port}`));
+const server = app.listen(port, () =>
+  winston.info(`listening on port ${port}`)
+);
 
 module.exports = server;
